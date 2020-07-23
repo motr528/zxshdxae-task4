d8003362b2d9f601da514db3dafee721a4ad3961
Maksim Iandulskii <jandulm@gmail.com>
2020-07-23 14:35:16 +0300

    Part1

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -10,7 +10,7 @@ public class Part1 {
     private static final String LINE_SEP = System.lineSeparator();
 
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("1.txt"),"windows-1251"))) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("part1.txt")))) {
             System.setOut(new PrintStream(System.out, true, "UTF-8"));
             String s;
             StringBuilder sb = new StringBuilder();
