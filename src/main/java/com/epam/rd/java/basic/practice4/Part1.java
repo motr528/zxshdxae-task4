0f018ce2bf056f994b179ab4dac9d9dadce3a7dc
Maksim Iandulskii <jandulm@gmail.com>
2020-07-23 14:45:40 +0300

    Part1

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,8 +1,6 @@
 package com.epam.rd.java.basic.practice4;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,20 +8,23 @@ public class Part1 {
     private static final String LINE_SEP = System.lineSeparator();
 
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("part1.txt"),"windows-1251"))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("part1.txt"), "windows-1251"))) {
             System.setOut(new PrintStream(System.out, true, "UTF-8"));
             String s;
             StringBuilder sb = new StringBuilder();
             while ((s = br.readLine()) != null) {
                 sb.append(s);
-                sb.append(LINE_SEP);
+                if (br.ready()) {
+                    sb.append(LINE_SEP);
+                }
+
             }
 
             s = sb.toString();
             Matcher matcher = Pattern.compile("[\\w\\p{L}]{4,}").matcher(s);
 
             while (matcher.find()) {
-                s = s.replace(matcher.group(),matcher.group().substring(2));
+                s = s.replace(matcher.group(), matcher.group().substring(2));
             }
 
             System.out.println(s);
