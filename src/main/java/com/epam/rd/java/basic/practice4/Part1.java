62fae34c82dadeb391aad0b38e77c58ea39ab5c4
Maksim Iandulskii <jandulm@gmail.com>
2020-07-23 14:27:38 +0300

    Part1

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,9 +1,36 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part1 {
+    private static final String LINE_SEP = System.lineSeparator();
 
     public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("1.txt"),"windows-1251"))) {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+            String s;
+            StringBuilder sb = new StringBuilder();
+            while ((s = br.readLine()) != null) {
+                sb.append(s);
+                sb.append(LINE_SEP);
+            }
 
+            s = sb.toString();
+            Matcher matcher = Pattern.compile("[\\w\\p{L}]{4,}").matcher(s);
+
+            while (matcher.find()) {
+                s = s.replace(matcher.group(),matcher.group().substring(2));
+            }
+
+            System.out.println(s);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
